dac283e90c9f3877a4934c203c06e596010f451a
Daniel Laurindo <dslaurindo@gmail.com>
2022-04-19 18:28:31 -0300

    Testing heroku deployment

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const path = require('path');
 const express = require('express');
 const app = express();
-const publicPath = path.join(__dirname, '..', 'public');
+const publicPath = path.join(__dirname, '..', 'build');
 const port = process.env.PORT || 3000;
 app.use(express.static(publicPath));
+app.use(express.static('build'));
+
 app.get('*', (req, res) => {
    res.sendFile(path.join(publicPath, 'index.html'));
 });
