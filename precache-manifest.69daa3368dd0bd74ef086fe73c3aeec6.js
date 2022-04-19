5cf40f8be943d9fb4263d14cada854c90a783924
Daniel Laurindo <dslaurindo@gmail.com>
2022-04-19 16:49:08 -0300

    Updates

---
+++ precache-manifest.69daa3368dd0bd74ef086fe73c3aeec6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cda77ebf4122ddbf2f227dbcd5649f5e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "68a63d2cd6cae6f6d9d2",
+    "url": "/static/css/main.7465e14d.chunk.css"
+  },
+  {
+    "revision": "399789c198712a0c3cae",
+    "url": "/static/js/2.f4703c3f.chunk.js"
+  },
+  {
+    "revision": "385801f39fe2615b5790da562b481c6c",
+    "url": "/static/js/2.f4703c3f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "68a63d2cd6cae6f6d9d2",
+    "url": "/static/js/main.05e84f58.chunk.js"
+  },
+  {
+    "revision": "b7b56a56fbb63b549511",
+    "url": "/static/js/runtime-main.e3060a99.js"
+  }
+]);
